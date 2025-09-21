fa7cb0a345f19e35956ce79f5ae1337b79c50cb0
GeneJackett <genejackett@gmail.com>
2025-09-20 22:39:47 -0500

    Initial commit

---
+++ src/main/java/com/personalsite/wesleygene/WesleygeneApplication.java
@@ -0,0 +1,13 @@
+package com.personalsite.wesleygene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesleygeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesleygeneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalsite/wesleygene/WesleygeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalsite.wesleygene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WesleygeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
